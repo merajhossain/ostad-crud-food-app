df557de1632ca818c5e7d896bb5129e1470af1da
Meraj Hossain <merajhossain29@gmail.com>
2024-04-24 09:24:59 +0600

    ui

--- food-app-frontend/src/main.jsx
+++ food-app-frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import  'bootstrap/dist/css/bootstrap.css';
+import  'bootstrap-icons/font/bootstrap-icons.css';
+import  './assets/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
